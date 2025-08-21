12e0601245e9a1283ea36a2c5e5200ef3a5fa956
Bitupanbiditchutia <bitupanbiditchutia@gmail.com>
2025-08-21 08:23:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+var typed = new Typed(".text", {
+    strings: ["Web Developer", "Microsoft Office Expert", "Adobe Photoshop Expert"],
+    typeSpeed: 100,   // correct property name
+    backSpeed: 100,
+    backDelay: 1000,
+    loop: true
+});
+
